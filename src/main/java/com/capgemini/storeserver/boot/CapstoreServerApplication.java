b5737400d9c7f5eaa18a6aa08d3d6972174180ac
Lalit Nikhare <lalitn246@gmail.com>
2018-08-17 14:28:03 +0530

    Initial project Upload

---
+++ src/main/java/com/capgemini/storeserver/boot/CapstoreServerApplication.java
@@ -0,0 +1,12 @@
+package com.capgemini.storeserver.boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CapstoreServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CapstoreServerApplication.class, args);
+	}
+}
