21f58f0f832be0a1d870f30c30990e0ef5ffab6b
AbhishekBipalliwar19 <ADM-IG-HWDLAB1C@PUNHWD12267.corp.capgemini.com>
2018-08-18 16:33:53 +0530

    Added UpdateCategory and getAllProducts Functionality

--- src/main/java/com/capgemini/storeserver/services/AdminServices.java
+++ src/main/java/com/capgemini/storeserver/services/AdminServices.java
@@ -1,6 +1,10 @@
 package com.capgemini.storeserver.services;
 
+import java.util.List;
+
+import com.capgemini.storeserver.beans.Category;
 import com.capgemini.storeserver.beans.Merchant;
+import com.capgemini.storeserver.beans.Product;
 
 
 public interface AdminServices {
@@ -8,4 +12,8 @@ public interface AdminServices {
 	public Merchant addMerchant(Merchant merchant);
 	
 	public void removeMerchant(int id);
+	
+public List<Product> viewAllProducts();
+	
+	Category updateCategory(int categoryId, String categoryName,String type);
 }
--- src/main/java/com/capgemini/storeserver/services/AdminServicesImpl.java
+++ src/main/java/com/capgemini/storeserver/services/AdminServicesImpl.java
@@ -1,11 +1,18 @@
 package com.capgemini.storeserver.services;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import com.capgemini.storeserver.beans.Category;
 import com.capgemini.storeserver.beans.Merchant;
+import com.capgemini.storeserver.beans.Product;
 import com.capgemini.storeserver.repo.AdminRepo;
+import com.capgemini.storeserver.repo.CategoryRepo;
 import com.capgemini.storeserver.repo.MerchantRepo;
+import com.capgemini.storeserver.repo.ProductRepo;
 
 @Service(value="adminServices")
 public class AdminServicesImpl implements AdminServices {
@@ -27,5 +34,28 @@ public class AdminServicesImpl implements AdminServices {
 	public void removeMerchant(int merchantId) {
 
 		merchantRepo.deleteById(merchantId);
+	}
+	
+	@Autowired
+	private CategoryRepo categoryRepo;
+	
+	@Autowired
+	private ProductRepo productRepo;
+	
+	@Override
+	public List<Product> viewAllProducts() {
+		System.out.println(productRepo.findAll());
+
+		return productRepo.findAll();
+	}
+
+	@Override
+	@Transactional
+	public Category updateCategory(int categoryId, String categoryName, String type) {
+		Category category = categoryRepo.updateCategory(categoryId);
+		category.setCategoryName(categoryName);
+		category.setType(type);
+		return category;
+
 	}
 }
